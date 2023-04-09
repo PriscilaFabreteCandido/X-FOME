cdf6b35b4b9dd9e2e2134bdd82a0a46c57aa98ff
Priscila Candido <priscila.candido@vixteam.com.br>
2023-04-09 14:03:06 -0300

    Primeiro commit

--- x-fome/src/main/java/com/mycompany/x/fome/domain/Carrinho.java
+++ x-fome/src/main/java/com/mycompany/x/fome/domain/Carrinho.java
@@ -5,10 +5,32 @@
  */
 package com.mycompany.x.fome.domain;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 2021122760232
  */
 public class Carrinho {
     
+    private Pedido pedido;
+    private ArrayList<Produto> produtos;
+    private Desconto desconto = new DefaultDesconto(10.0);
+    
+    public Double getTotalWithDesconto(){
+        return this.desconto.getDesconto(this);
+    }
+
+    public void setDesconto(Desconto desconto) {
+        this.desconto = desconto;
+    }
+
+    public Double getTotal(){
+        Double total = 0.0;
+        for(Produto p: this.produtos){
+            total += p.getValor();
+        }
+        
+        return total;
+    }
 }
---
+++ x-fome/src/main/java/com/mycompany/x/fome/domain/Produto.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.x.fome.domain;
+
+/**
+ *
+ * @author prisc
+ */
+public class Produto {
+    private Integer idProduto;
+    private String nome;
+    private Double valor;
+
+    public Integer getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    
+}
