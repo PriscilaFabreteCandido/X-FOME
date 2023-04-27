6d7bb58c3df03cf1e913f3584b39db95655d23c5
2021122760232 <2021122760232@col-8953-li01.cefetes.br>
2023-04-27 16:05:38 -0300

    aa

--- x-fome/src/main/java/com/mycompany/x/fome/gertarefas/GerInterGrafica.java
+++ x-fome/src/main/java/com/mycompany/x/fome/gertarefas/GerInterGrafica.java
@@ -5,6 +5,13 @@
  */
 package com.mycompany.x.fome.gertarefas;
 
+import com.mycompany.x.fome.view.DlgLogin;
+import com.mycompany.x.fome.view.DlgLoja;
+import com.mycompany.x.fome.view.DlgPedido;
+import com.mycompany.x.fome.view.DlgUsuario;
+import com.mycompany.x.fome.view.DlgVisualizarPedidos;
+import com.mycompany.x.fome.view.FormGerenciarPedidos;
+import com.mycompany.x.fome.view.FormPrincipalCliente;
 import java.awt.Frame;
 import java.lang.reflect.InvocationTargetException;
 import javax.swing.JDialog;
@@ -15,8 +22,17 @@ import javax.swing.JOptionPane;
  * @author 2021122760232
  */
 public class GerInterGrafica {
-
-     public GerInterGrafica() {
+    
+    private DlgLogin dlgLogin = null;
+    private DlgLoja dlgLoja = null;
+    private DlgPedido dlgPedido = null;
+    private DlgUsuario dlgUsuario = null;
+    private DlgVisualizarPedidos dlgDlgVisualizarPedidos = null;
+    private FormGerenciarPedidos gerenciarPedidos = null;
+    private FormPrincipalCliente telaPrincipalCliente = null;
+    
+            
+    public GerInterGrafica() {
         
     }
    
@@ -34,53 +50,33 @@ public class GerInterGrafica {
     }
     
     public void openJanelaLogin(){
-        
+        dlgLogin = (DlgLogin) abrirJanela(null, dlgLogin, DlgLogin.class);
     }
     
     public void openJanelaLoja(){
-        
+        dlgLoja = (DlgLoja) abrirJanela(telaPrincipalCliente, dlgLoja, DlgLoja.class);
     }
     
     public void openJanelaUsuario(){
-        
+        dlgUsuario = (DlgUsuario) abrirJanela(null, dlgUsuario, DlgUsuario.class);
     }
     
     public void openJanelaVisualizarPedidos(){
-        
+        dlgDlgVisualizarPedidos = (DlgVisualizarPedidos) abrirJanela(telaPrincipalCliente, dlgDlgVisualizarPedidos, DlgVisualizarPedidos.class);
     }
     
     public void openJanelaPrincipalDoCliente(){
-        
+        gerenciarPedidos = new FormGerenciarPedidos();
+        gerenciarPedidos.setVisible(true);
     }
     
     public void openJanelaGerenciarPedidos(){
-        
+        gerenciarPedidos = new FormGerenciarPedidos();
+        gerenciarPedidos.setVisible(true);
     }
+    
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                // System.out.println( info.getName() );
-                if ("Windows".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
+  
         // TRADUÇÃO
         javax.swing.UIManager.put("OptionPane.yesButtonText", "Sim"); 
         javax.swing.UIManager.put("OptionPane.noButtonText", "Não");
